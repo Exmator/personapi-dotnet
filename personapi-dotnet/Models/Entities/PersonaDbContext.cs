1953015bc0ad6676d89a94721d313fca3e8fd318
Nicolas Algarra <nicolasalgarrapolanco@gmail.com>
2026-05-09 21:32:29 -0500

    feat: Entities creation

---
+++ personapi-dotnet/Models/Entities/PersonaDbContext.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace personapi_dotnet.Models.Entities;
+
+public partial class PersonaDbContext : DbContext
+{
+    public PersonaDbContext()
+    {
+    }
+
+    public PersonaDbContext(DbContextOptions<PersonaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estudio> Estudios { get; set; }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    public virtual DbSet<Profesion> Profesions { get; set; }
+
+    public virtual DbSet<Telefono> Telefonos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Name=DefaultConnection");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estudio>(entity =>
+        {
+            entity.HasKey(e => new { e.IdProf, e.CcPer });
+
+            entity.ToTable("estudios");
+
+            entity.Property(e => e.IdProf).HasColumnName("id_prof");
+            entity.Property(e => e.CcPer).HasColumnName("cc_per");
+            entity.Property(e => e.Fecha).HasColumnName("fecha");
+            entity.Property(e => e.Univer)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("univer");
+
+            entity.HasOne(d => d.CcPerNavigation).WithMany(p => p.Estudios)
+                .HasForeignKey(d => d.CcPer)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_estudios_per");
+
+            entity.HasOne(d => d.IdProfNavigation).WithMany(p => p.Estudios)
+                .HasForeignKey(d => d.IdProf)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_estudios_prof");
+        });
+
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Cc);
+
+            entity.ToTable("persona");
+
+            entity.Property(e => e.Cc)
+                .ValueGeneratedNever()
+                .HasColumnName("cc");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Genero)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("genero");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Profesion>(entity =>
+        {
+            entity.ToTable("profesion");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Des)
+                .IsUnicode(false)
+                .HasColumnName("des");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(90)
+                .IsUnicode(false)
+                .HasColumnName("nom");
+        });
+
+        modelBuilder.Entity<Telefono>(entity =>
+        {
+            entity.HasKey(e => e.Num);
+
+            entity.ToTable("telefono");
+
+            entity.Property(e => e.Num)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("num");
+            entity.Property(e => e.Duenio).HasColumnName("duenio");
+            entity.Property(e => e.Oper)
+                .HasMaxLength(45)
+                .IsUnicode(false)
+                .HasColumnName("oper");
+
+            entity.HasOne(d => d.DuenioNavigation).WithMany(p => p.Telefonos)
+                .HasForeignKey(d => d.Duenio)
+                .HasConstraintName("FK_telefono_per");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
